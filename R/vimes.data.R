6c3fe3e881985811a6a34ec370e061d929fd9bba
Thibaut <thibautjombart@gmail.com>
2016-01-20 15:35:02 +0000

    changed engine for vignette

--- R/vimes.data.R
+++ R/vimes.data.R
@@ -5,29 +5,30 @@
 #' @param ... a set of matrices or dist objects serving as input
 #'
 #' @export
+#' @importFrom stats as.dist
 #'
 vimes.data <- function(...){
-    ## EXTRACT DATA FROM LIST ##
+    ## PROCESS INPUT ##
+    ## extract data from list ##
     data <- list(...)
 
-    ## CONVERT DATA TO MATRICES ##
+    ## convert data to matrices ##
     data <- lapply(data, as.dist)
 
-    ## ADD LABELS IF NEEDED ##
+    ## add labels if needed ##
     for(i in seq_along(data)){
         if(is.null(labels(data[[i]]))) attr(data[[i]], "Labels") <- 1:attr(data[[i]], "Size")
     }
 
-    ## MATCH MATRICES ##
-    ## get list of all labels
+    ## MATCH ORDERING/FILL GAPS ##
+    ## get list of all labels ##
     all.labels <- unique(unlist(lapply(data, attr, "Labels")))
 
-    ## reorder dist objects
+    ## reorder dist objects ##
     out <- list()
     for(i in seq_along(data)){
         out[[i]] <- as.dist(as.matrix(data[[i]])[all.labels, all.labels])
     }
-
 
     ## RETURN OUTPUT ##
     attr(out, "labels") <- all.labels
