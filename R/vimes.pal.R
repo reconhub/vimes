40340008eb051626f1146dea7947b14d23b6688b
Thibaut <thibautjombart@gmail.com>
2016-01-20 17:43:04 +0000

    changing palette behaviour

--- R/vimes.pal.R
+++ R/vimes.pal.R
@@ -11,8 +11,15 @@
 #' @examples
 #'
 #' plot(1:8, cex=8, pch=20, col=vimes.pal1(8), main="vimes.pal1")
-#'
-vimes.pal1 <- colorRampPalette(
-    c("#ff6666", "#666699","#79d2a6",
-      "#a4a4c1","#ffcc00","#85e085",
-      "#df9fbf","#b2b266"))
+#' plot(1:4, cex=8, pch=20, col=vimes.pal1(4), main="vimes.pal1")
+#' plot(1:100, col=vimes.pal1(100), pch=20, cex=6)
+vimes.pal1 <- function(n){
+    colors <- c("#ff6666", "#666699","#79d2a6",
+                "#a4a4c1","#ffcc00","#85e085",
+                "#df9fbf","#b2b266")
+    if(n<8) {
+        return(colors[seq_len(n)])
+    } else {
+        return(colorRampPalette(colors)(n))
+    }
+}
