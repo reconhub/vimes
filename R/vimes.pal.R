a72b87faeede2a3910fa4cb1995eea420e0b328a
Thibaut <thibautjombart@gmail.com>
2016-01-21 18:51:18 +0000

    adding new palette

--- R/vimes.pal.R
+++ R/vimes.pal.R
@@ -6,19 +6,35 @@
 #'
 #' @param n a number of colors
 #'
+#' @rdname palettes
+#' @aliases palettes vimes.pal1 vimes.pal2
+#'
 #' @export
 #' @importFrom grDevices colorRampPalette
 #'
 #' @examples
 #'
-#' plot(1:8, cex=8, pch=20, col=vimes.pal1(8), main="vimes.pal1")
 #' plot(1:4, cex=8, pch=20, col=vimes.pal1(4), main="vimes.pal1")
-#' plot(1:100, col=vimes.pal1(100), pch=20, cex=6)
+#' plot(1:20, col=vimes.pal1(20), pch=20, cex=6, main="vimes.pal1")
+#' plot(1:10, col=vimes.pal2(10), pch=20, cex=6, main="vimes.pal2")
 vimes.pal1 <- function(n){
-    colors <- c("#ff6666", "#666699","#79d2a6",
-                "#a4a4c1","#ffcc00","#85e085",
-                "#df9fbf","#b2b266")
-    if(n<8) {
+    colors <- c("#ff6666", "#666699")
+    if(n<length(colors)) {
+        return(colors[seq_len(n)])
+    } else {
+        return(colorRampPalette(colors)(n))
+    }
+}
+
+
+#' @rdname palettes
+#' @export
+vimes.pal2 <- function(n){
+    colors <- c("#ccddff", "#79d2a6", "#ffb3b3",
+                "#a4a4c1","#ffcc00", "#ff9f80",
+                "#ccff99", "#df9fbf","#ffcc99",
+                "#cdcdcd")
+    if(n<length(colors)) {
         return(colors[seq_len(n)])
     } else {
         return(colorRampPalette(colors)(n))
