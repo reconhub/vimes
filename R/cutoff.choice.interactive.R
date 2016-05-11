6f52a67c4ab89034343287237150a62d8511fb45
Thibaut <thibautjombart@gmail.com>
2016-05-11 12:37:41 +0100

    put cutoff choice in separate file.

---
+++ R/cutoff.choice.interactive.R
@@ -0,0 +1,41 @@
+#' Interactive choice of cutoff points.
+#'
+#' This function displays histograms of pairwise distances and asks the user for a cut-off point to choose. It will prune each corresponding graph for any new value requested.
+#'
+#' @author Thibaut Jombart \email{thibautjombart@@gmail.com}
+#'
+#' @inheritParams vimes 
+#' @param ... further arguments passed to 'hist'
+#' 
+cutoff.choice.interactive <- function(x, graph.opt=vimes.graph.opt(), ...){
+      chooseAgain <- TRUE
+        while (chooseAgain) {
+            ## plot histogram ##
+            hist(x, xlab="Pairwise distances", ylab="Frequency", main="Choose a cutoff distance",
+                 border="white", col="#8585ad", ...)
+
+            ## get input from user ##
+            cat("\nEnter a cutoff distance:  ")
+            cutoff <- NA
+            while(is.null(cutoff) || is.na(cutoff)) suppressWarnings(cutoff <- as.numeric(readLines(n = 1)))
+
+            ## add cutoff to the plot ##
+            abline(v=cutoff,col="red",lty=2, lwd=2)
+
+            ## get corresponding output ##
+            out <- vimes.prune(x, cutoff=cutoff, graph.opt=graph.opt)
+
+            ## show output ##
+            cat(paste("\nNumber of clusters found:  ", out$clusters$K, sep=""))
+            plot(out$graph)
+
+            ## ask if cutoff should be kept ##
+            ans <- ""
+            while(!ans %in% c("y","n")){
+                cat("\nAre you satisfied with this solution? (yes:y / no:n): ")
+                ans <- tolower(readLines(n = 1))
+            }
+            if(ans=="y") chooseAgain <- FALSE
+        }
+        return(out)
+}
--- R/vimes.prune.R
+++ R/vimes.prune.R
@@ -34,7 +34,7 @@ vimes.prune <- function(x, cutoff=NULL, graph.opt=vimes.graph.opt(), ...){
 
     ## INTERACTIVE MODE FOR CHOOSING CUTOFF ##
     if(is.null(cutoff)){
-        return(cutoff.choice(x=x, graph.opt=graph.opt))
+        return(cutoff.choice.interactive(x=x, graph.opt=graph.opt))
     }
 
 
@@ -63,36 +63,3 @@ vimes.prune <- function(x, cutoff=NULL, graph.opt=vimes.graph.opt(), ...){
 
 
 
-## UNEXPORTED FUNCTION FOR INTERACTIVE CUTOFF ##
-cutoff.choice <- function(x, graph.opt, ...){
-      chooseAgain <- TRUE
-        while (chooseAgain) {
-            ## plot histogram ##
-            hist(x, xlab="Pairwise distances", ylab="Frequency", main="Choose a cutoff distance",
-                 border="white", col="#8585ad", ...)
-
-            ## get input from user ##
-            cat("\nEnter a cutoff distance:  ")
-            cutoff <- NA
-            while(is.null(cutoff) || is.na(cutoff)) suppressWarnings(cutoff <- as.numeric(readLines(n = 1)))
-
-            ## add cutoff to the plot ##
-            abline(v=cutoff,col="red",lty=2, lwd=2)
-
-            ## get corresponding output ##
-            out <- vimes.prune(x, cutoff=cutoff, graph.opt=graph.opt)
-
-            ## show output ##
-            cat(paste("\nNumber of clusters found:  ", out$clusters$K, sep=""))
-            plot(out$graph)
-
-            ## ask if cutoff should be kept ##
-            ans <- ""
-            while(!ans %in% c("y","n")){
-                cat("\nAre you satisfied with this solution? (yes:y / no:n): ")
-                ans <- tolower(readLines(n = 1))
-            }
-            if(ans=="y") chooseAgain <- FALSE
-        }
-        return(out)
-} # end cutoff.choice
