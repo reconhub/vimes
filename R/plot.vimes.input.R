961ac4e1d12befc7cb55b79a5256e206c6649e1f
Thibaut <thibautjombart@gmail.com>
2016-05-11 12:06:36 +0100

    improving comments

--- R/plot.vimes.input.R
+++ R/plot.vimes.input.R
@@ -28,19 +28,29 @@
 #'
 plot.vimes.input <- function(x, y=NULL, col=vimes.pal1(length(x)),
                              n=length(x), which=1:length(x), ...){
-    ## CHECK ARGUMENTS ##
+    
+    ## Some basic processing of the arguments: selecting data to plot,
+    ## generating material required for plotting such as color etc.
+    
     x <- x[which]
     N <- length(x)
     if(N<1) stop("x is empty")
     if(n<1) stop("n is less than 1")
     col <- rep(col, length=N)
 
-    ## MAKE PLOTS ##
+    
+    ## Plotting will be a simple series of histograms
+    
     par(mfrow=c(n,1))
     for(i in seq_along(x)){
         lab <- ifelse(is.null(names(x)[i]), paste("Distance",i), names(x)[i])
         hist(x[[i]], main=lab, col=col[i], border="white", xlab="Pairwise distance", ...)
     }
 
+    
+    ## This kind of thing might upset some. Rich, if you are reading
+    ## these lines, here is my attempt at soothing your anger:
+    ## https://www.youtube.com/watch?v=3ZsUmoZaivg
+
     return(invisible(NULL))
-} # end plot.vimes.input
+}
