81197bf5cad822e579c33d2890f1ce9f06b7eaff
Thibaut Jombart <thibautjombart@gmail.com>
2016-05-19 07:17:27 +0100

    vimes.dist files

---
+++ R/vimes.dist.R
@@ -0,0 +1,93 @@
+#' Process input distances for vimes
+#'
+#' This function takes a series of 'dist' objects (or objects which can be converted to 'dist', and ensures that entries match in all objects, adding NAs where necessary. The total number of cases and labels are returned as attributes.
+#'
+#' @author Thibaut Jombart \email{thibautjombart@@gmail.com}
+#'
+#' @param ... a series (alternatively, a list) of matrices or dist objects serving as input.
+#'
+#' @export
+#'
+#' @return a list of dist objects with matching entries, with attributes: 'labels' (labels of the cases) and 'N' (number of cases)
+#'
+#' @seealso \code{\link{vimes.data}} to process raw data (e.g. dates, locations, genetic sequences)
+#'
+#' @examples
+#' x1 <- c(0,1,3)
+#' x2 <- c(2,5)
+#' names(x1) <- letters[1:3]
+#' names(x2) <- c('a', 'r')
+#' D1 <- dist(x1)
+#' D2 <- dist(x2)
+#' out <- vimes.dist(D1, D2)
+#' out
+#'
+vimes.dist <- function(...){
+    ## Data passed through ... are meant to be pairwise distances
+    ## between labelled cases; we process these inputs by i) turning
+    ## them into matrices ii) ensuring matching of labels iii)
+    ## converting data into 'dist' objects. Final labels are stored as
+    ## attributes of the returned list of 'dist' objects.
+
+    ## PROCESS TYPES OF INPUT  ##
+    ## extract data from list ##
+    data <- list(...)
+    data.names <- names(data)
+    if(length(data)==0L) stop("no data to process")
+
+    ## escape if data has been processed already
+    if(inherits(data[[1]],"vimes.input")) return(data[[1]])
+
+    ## if first item is a list, use it as input
+    if(is.list(data[[1]])) data <- data[[1]]
+
+
+    ## ENSURE MATRICES AND LABELLING ##
+    ## convert all data to matrices
+    data <- lapply(data, as.matrix)
+    K <- length(data)
+
+    ## assign labels if missing
+    for(i in seq_along(data)){
+        if(is.null(rownames(data[[i]]))) {
+            rownames(data[[i]]) <- colnames(data[[i]]) <- 1:nrow(data[[i]])
+        }
+    }
+
+
+    ## HANDLE NAS AND SORTING ##
+
+    ## The policy will be to remove cases with NAs, as cases with NA
+    ## distances tend to link all other cases. This may change in
+    ## later version of the method.
+
+    ## get labels to keep
+    ## (i.e. present without NA everywhere)
+    lab.to.keep <- Reduce(intersect,
+                          lapply(data, function(e) rownames(e)[!apply(is.na(e),1,all)])
+                          )
+    N <- length(lab.to.keep)
+    if(N<2){
+        warning("Data contain less than 2 cases without missing data.")
+        return(NULL)
+    }
+
+    ## remove NAs, order, store result
+    out <- vector(K, mode="list")
+    for(i in seq_along(data)){
+        out[[i]] <- stats::as.dist(data[[i]][lab.to.keep, lab.to.keep])
+    }
+
+
+    ## RETURN OUTPUT ##
+
+    ## Output will be a list of 'dist' objects; labels and the number
+    ## of cases will be stored as attributes; the output will be a
+    ## list with the additional class vimes.input
+    names(out) <- data.names
+    attr(out, "labels") <- lab.to.keep
+    attr(out, "N") <- N
+
+    class(out) <- c("list", "vimes.input")
+    return(out)
+}
