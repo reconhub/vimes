0a9006fda3c07991fdc859011076bf475cb21530
thibautjombart <thibautjombart@gmail.com>
2017-02-24 17:36:30 +0000

    adding new code file

---
+++ R/distributions.R
@@ -0,0 +1,67 @@
+#' Expected distributions of distances
+#'
+#' These functions compute the expected distributions of distances between pairs
+#' of cases given a case reporting probability 'pi'. Analytical results are used
+#' for some special cases, including:
+#'
+#' \itemize{
+#'
+#' \item temporal distances: the serial interval is assumed to be
+#' gamma-distributed; the procedure returns a discretized, weighted convolution
+#' of gamma distributions.
+#'
+#' \item genetic distances: assumed ... TBC
+#'
+#' }
+#'
+#'
+#' @author Anne Cori (a.cori@@imperial.ac.uk) and Thibaut Jombart
+#' (thibautjombart@@gmail.com).
+#' 
+#' @export
+#'
+#' @rdname distributions
+#'
+
+dpaircase <- function() {
+
+}
+
+
+
+
+
+
+#' @export
+#' @rdname distributions
+dtemporal <- function() {
+
+}
+
+
+
+
+
+
+#' @export
+#' @rdname distributions
+dspatial <- function() {
+
+}
+
+
+
+
+
+
+#' @export
+#' @rdname distributions
+dgenetic <- function() {
+
+}
+
+
+
+
+
+
