db8ce4e0f31782b70972de0d6eed3e8840e2f6e5
Thibaut Jombart <thibautjombart@gmail.com>
2017-02-28 11:21:20 +0000

    first working version of dempirical

--- R/convol_internals.R
+++ R/convol_internals.R
@@ -122,10 +122,7 @@ get_max_kappa <- function(pi, alpha = 0.001) {
 ## re-standardise values to have actual weights summing to 1. 'alpha' is the
 ## threshold probability used to determine the maximum value of 'kappa'.
 
-get_weights <- function(pi, alpha) {
-  pi <- check_pi(pi)
-  max_kappa <- get_max_kappa(pi, alpha)
-
+get_weights <- function(pi, max_kappa) {
   x_val <- seq_len(max_kappa)
 
   out <- stats::dgeom(x_val - 1, pi)
@@ -192,7 +189,7 @@ convolve_spatial <- function(sd, kappa) {
 
 
 ## This convolution supposes that the user has specified a probability mass
-## function. Checks and standardisation are achieved by check_pmf. The argument
+## function. Prior checks and standardisation are achieved by check_pmf. The argument
 ## 'keep_all' triggers different behaviours and outputs:
 
 ## - keep_all = FALSE: returns the 'kappa' convolution of the pmf
@@ -201,10 +198,13 @@ convolve_spatial <- function(sd, kappa) {
 ## - so, it makes sure that all vectors have the same length.
 
 convolve_empirical <- function(x, kappa, keep_all = FALSE) {
-  x <- check_pmf(x)
   kappa <- check_kappa(kappa, only_one = TRUE)
 
   if (kappa == 1) {
+    if (keep_all) {
+      x <- matrix(x)
+      colnames(x) <- "1"
+    }
     return(x)
   }
 
@@ -214,13 +214,24 @@ convolve_empirical <- function(x, kappa, keep_all = FALSE) {
   if (keep_all) {
     out <- list()
     out[[1]] <- x
+
     for (k in 2:kappa) {
       out[[k]] <- stats::convolve(out[[k-1]],
                                   rev(x),
                                   type="open")
     }
+
+
+    ## Here we shape the output as a data.frame where each column is a different
+    ## value of kappa.
+
+    L <- length(out[[kappa]])
+    out <- lapply(out, fill_with, 0, L)
+    out <- as.matrix(data.frame(out))
+    colnames(out) <- seq_len(ncol(out))
   } else {
     out <- x
+
     for (k in 2:kappa) {
       out <- stats::convolve(out,
                              rev(x),
--- R/distributions.R
+++ R/distributions.R
@@ -17,24 +17,17 @@
 #'
 #' @author Anne Cori (a.cori@@imperial.ac.uk) and Thibaut Jombart
 #' (thibautjombart@@gmail.com).
-#' 
+#'
 #' @export
 #'
 #' @rdname distributions
 #'
+#' @param alpha The probability threshold to be used to determine the maximum
+#'   value of kappa; this value ('max_kappa') will be the smallest kappa so that
+#'   p(kappa > max_kappa) < alpha. Defaults to 0.001.
+#'
 
-dpaircase <- function() {
-
-}
-
-
-
-
-
-
-#' @export
-#' @rdname distributions
-dtemporal <- function() {
+dpaircase <- function(alpha = 0.001) {
 
 }
 
@@ -44,8 +37,10 @@ dtemporal <- function() {
 
 
 #' @export
+#'
 #' @rdname distributions
-dspatial <- function() {
+#'
+dtemporal <- function(alpha = 0.001) {
 
 }
 
@@ -55,9 +50,48 @@ dspatial <- function() {
 
 
 #' @export
+#'
 #' @rdname distributions
-dgenetic <- function() {
+#'
+dspatial <- function(alpha = 0.001) {
 
+}
+
+
+
+
+
+
+#' @export
+#'
+#' @rdname distributions
+#'
+dgenetic <- function(alpha = 0.001) {
+
+}
+
+
+
+
+
+
+#' @export
+#'
+#' @rdname distributions
+#'
+#' @param x A \code{numeric} vector providing the probability mass function, or
+#'   empirical frequencies, of pairwise distances.
+#'
+dempiric <- function(x, pi, alpha = 0.001) {
+  pi <- check_pi(pi)
+  x <- check_pmf(x)
+
+  max_kappa <- get_max_kappa(pi, alpha)
+  weights <- get_weights(pi, max_kappa)
+  distributions <- convolve_empirical(x, max_kappa, TRUE)
+
+  out <- distributions %*% weights
+  return(as.vector(out))
 }
 
 
