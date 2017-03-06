b7655803fffceeec45c61837c8b421ea7ab7fc08
Anne Cori <a.cori@imperial.ac.uk>
2017-03-06 15:06:46 +0000

    now convolve_gamma uses keep_all argument properly, which will be useful to sum integrate results across all kappas from 1 to max_kappa

--- R/convol_internals.R
+++ R/convol_internals.R
@@ -25,23 +25,23 @@ check_kappa <- function(kappa, only_one = FALSE) {
   if (!is.numeric(kappa)) {
     stop("kappa must be numeric.")
   }
-
+  
   if (any(!is.finite(kappa))) {
     stop("Non-finite values in kappa.")
   }
-
+  
   if(any(kappa < 1)) {
     stop("kappa must be strictly positive.")
   }
-
+  
   kappa <- as.integer(round(kappa))
-
+  
   if (only_one) {
     if (length(kappa) != 1L) {
       stop("Only one value of kappa expected.")
     }
   }
-
+  
   return(kappa)
 }
 
@@ -71,7 +71,7 @@ get_max_kappa <- function(pi, alpha = 0.001) {
 
 get_weights <- function(pi, max_kappa) {
   x_val <- seq_len(max_kappa)
-
+  
   out <- stats::dgeom(x_val - 1, pi)
   out <- out / sum(out)
   return(out)
@@ -87,24 +87,37 @@ get_weights <- function(pi, max_kappa) {
 ## the sum of k independant Gamma distributed random variables with shape a and scale b
 ## is Gamma distributed with shape k*a and scale b. 
 
-## Note that kappa in the next 3 function, kappa is a single value
-## TO DO improve so kappa can be a vector of values. 
+## The argument
+## 'keep_all' triggers different behaviours and outputs:
+##    - keep_all = FALSE: returns the 'kappa' convolution of the pmf
+##    - keep_all = TRUE: returns all convolutions of the pmf from 1 to kappa;
 
 convolve_gamma <- function(shape, rate = 1, scale = 1/rate, kappa, keep_all = FALSE) { 
-  ## at the moment not vectorized in x, need to work on that
-  ## could potentially be done using 
-  ## f <- function(x) sapply(x, function(e) stats::dgamma(e, kappa*shape, rate))
-  ## but need to work on column/row names to clarify what's x and what's the gamma parameters
-    kappa <- check_kappa(kappa, only_one = FALSE) # allow kappa to be a vector
-
+  kappa <- check_kappa(kappa, only_one = TRUE) 
+  
+  if(keep_all)
+  {
+    f <- function(x) t(sapply(x, function(e) stats::dgamma(e, shape=(1:kappa)*shape, rate=rate)))
+  }else
+  {
     f <- function(x) stats::dgamma(x, kappa*shape, rate) 
-    return(f) 
+    
+  }
+  return(f) 
 }
 ## the convolution weighted by the geometric weights should eventually look like something like that:
-## weighted_convolve_gamma <- function(x, shape, rate = 1, scale = 1/rate, pi, max_kappa = 20) 
-## {
-##  sum(get_weights(pi, max_kappa)*convolve_gamma(shape, rate=rate, kappa=1:max_kappa)(x))
-## }
+# weighted_convolve_gamma <- function(x, shape, rate = 1, scale = 1/rate, pi, alpha = 0.001) 
+# {
+#   pi <- check_one_proba(pi)
+#   alpha <- check_one_proba(alpha)
+#   
+#   max_kappa <- get_max_kappa(pi, alpha)
+#   weights <- get_weights(pi, max_kappa)
+#   distributions <- convolve_gamma(shape, rate=rate, kappa=max_kappa, keep_all=TRUE)(x)
+#   
+#   out <- distributions %*% weights
+#   return(as.vector(out))
+# }
 
 
 
@@ -118,11 +131,11 @@ convolve_gamma <- function(shape, rate = 1, scale = 1/rate, kappa, keep_all = FA
 
 
 convolve_gamma_poisson <- function(gamma_shape, gamma_rate = 1, gamma_scale = 1/gamma_rate, poisson_rate, kappa, keep_all = FALSE) {
-    kappa <- check_kappa(kappa, only_one = FALSE)  # allow kappa to be a vector
-
-    prob <- 1-gamma_scale*poisson_rate/(gamma_scale*poisson_rate+1) # using prob = 1-p intead of p so that our definition correponds to that of rnbinom
-    f <- function(x) stats::dnbinom(x,size=gamma_shape*kappa,prob=prob)
-    return(f)
+  kappa <- check_kappa(kappa, only_one = TRUE) 
+  
+  prob <- 1-gamma_scale*poisson_rate/(gamma_scale*poisson_rate+1) # using prob = 1-p intead of p so that our definition correponds to that of rnbinom
+  f <- function(x) stats::dnbinom(x,size=gamma_shape*kappa,prob=prob)
+  return(f)
 }
 # TO DO: add a test to check that convolve_gamma_poisson(gamma_shape=gamma_shape, gamma_rate=gamma_rate, poisson_rate=poisson_rate, kappa=kappa)(x) is the same as: 
 # choose(gamma_shape*kappa+x-1, x)*(1-gamma_scale*poisson_rate/(gamma_scale*poisson_rate+1))^(gamma_shape*kappa)*(gamma_scale*poisson_rate/(gamma_scale*poisson_rate+1))^x
@@ -143,10 +156,11 @@ convolve_gamma_poisson <- function(gamma_shape, gamma_rate = 1, gamma_scale = 1/
 ## follows a Rayleigh distribution with scale s*sqrt(k)
 
 convolve_spatial <- function(sd, kappa, keep_all = FALSE) {
-    kappa <- check_kappa(kappa, only_one = FALSE) # allow kappa to be a vector
-
-    f <- function(x) VGAM::drayleigh(x,scale=sd*sqrt(kappa))
-    return(f)
+  kappa <- check_kappa(kappa, only_one = TRUE) 
+  
+  
+  f <- function(x) VGAM::drayleigh(x,scale=sd*sqrt(kappa))
+  return(f)
 }
 ## the convolution weighted by the geometric weights should eventually look like something like that:
 ## weighted_convolve_spatial <- function(x, sd, pi, max_kappa = 20) 
@@ -169,7 +183,7 @@ convolve_spatial <- function(sd, kappa, keep_all = FALSE) {
 
 convolve_empirical <- function(x, kappa, keep_all = FALSE) {
   kappa <- check_kappa(kappa, only_one = TRUE)
-
+  
   if (kappa == 1) {
     if (keep_all) {
       x <- matrix(x)
@@ -177,31 +191,31 @@ convolve_empirical <- function(x, kappa, keep_all = FALSE) {
     }
     return(x)
   }
-
-
+  
+  
   ## Computations should be checked by Anne
-
+  
   if (keep_all) {
     out <- list()
     out[[1]] <- x
-
+    
     for (k in 2:kappa) {
       out[[k]] <- stats::convolve(out[[k-1]],
                                   rev(x),
                                   type="open")
     }
-
-
+    
+    
     ## Here we shape the output as a data.frame where each column is a different
     ## value of kappa.
-
+    
     L <- length(out[[kappa]])
     out <- lapply(out, fill_with, 0, L)
     out <- as.matrix(data.frame(out))
     colnames(out) <- seq_len(ncol(out))
   } else {
     out <- x
-
+    
     for (k in 2:kappa) {
       out <- stats::convolve(out,
                              rev(x),
