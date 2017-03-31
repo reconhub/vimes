9789d000ea480a89af13ad6970a93937a6d18581
Thibaut Jombart <thibautjombart@gmail.com>
2017-03-31 12:18:13 +0100

    adding fpaircase and a print method

---
+++ R/fpaircase.R
@@ -0,0 +1,66 @@
+#' Expected distributions of distances (functions)
+#'
+#' The function \code{fpaircase} returns functions which compute the expected
+#' distributions of distances between pairs of cases given a case reporting
+#' probability 'pi'. See \code{\link{dpaircase}} for details on different types
+#' of distances between cases for which distributions can be computed.
+#'
+#'
+#' @author Thibaut Jombart \email{thibautjombart@@gmail.com}.
+#'
+#' @export
+#'
+#' @inheritParams dpaircase
+#'
+#' @examples
+
+fpaircase <- function(type = c("temporal","genetic","spatial", "empiric"),
+                      gamma_shape, gamma_rate = 1, gamma_scale = 1/gamma_rate,
+                      poisson_rate,
+                      sd_spatial,
+                      p,
+                      alpha = 0.001) {
+
+  out <- function(x, pi = 1) {
+    dpaircase(x, pi = pi, type = type,
+              gamma_shape = gamma_shape,
+              gamma_rate = gamma_rate,
+              gamma_scale = gamma_scale,
+              poisson_rate = poisson_rate,
+              sd_spatial = sd_spatial,
+              p = p,
+              alpha = alpha)
+  }
+
+  class(out) <- c("fpaircase", "function")
+  attr(out, "type") <- type
+  attr(out, "continuous") <- type %in% c("temporal", "spatial")
+  attr(out, "call") <- match.call()
+
+  return(out)
+}
+
+
+
+
+
+
+#' @export
+#'
+#' @rdname fpaircase
+#'
+#' @param ... further arguments passed to methods (print, plot, etc.)
+#'
+print.fpaircase <- function(x, ...) {
+  cat("/// distribution of distances")
+
+  cat(sprintf("\n  class: %s",
+              paste(class(x), collapse = ", ")))
+
+  cat(sprintf("\n  type: %s (%s) distribution",
+              attr(x, "type"),
+              ifelse(attr(x, "continuous"), "continuous", "discrete")))
+
+  cat("\n  original call:", deparse(attr(x, "call")))
+  cat("\n")
+}
