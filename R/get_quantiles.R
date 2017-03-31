86d9e9bf7cb0905b0f1b57aafdc71c6d597bcd78
Anne Cori <a.cori@imperial.ac.uk>
2017-03-31 15:24:39 +0100

    fixing get_quantiles_pdf to return Inf for 1 quantiles

--- R/get_quantiles.R
+++ R/get_quantiles.R
@@ -20,37 +20,48 @@
 get_quantiles_pdf <- function(f, p, precision = 0.01, n_steps = 1000, ...) {
   p <- check_proba(p)
   
-  ## we approximate the empirical cdf by stepsize precision, over first n_steps
-  ## steps
+  out <- rep(Inf, length(p))
   
-  x <- seq(from = 0, by = precision, length = n_steps)
-  csm <- cumsum(f(x, ...)) * precision
-  iter <- 0
+  p_no_1 <- p[p < 1]
   
-  
-  ## if we didn't go far enough to catch the quantile p (or at least one of
-  ## them) we reiterate the process as long as needed, by n_steps at a time
-  
-  while (max(p) > max(csm)) {
-    iter <- iter + 1
-    new_x <- seq(from = max(x)+precision, by = precision, length = n_steps)
-    x <- c(x, new_x)
-    new_cms <- cumsum(f(new_x, ...))*precision + max(csm)
-    csm <- c(csm, new_cms)
-  }
-  
-  
-  ## now we have the approximate cdf up to far enough, we find where the
-  ## quantile(s) p lie in the recorded vector of cdf quantile defined as the
-  ## smallest recorded cdf which is > p
-  
-  idx_q <- vapply(p, function(e){
-    tmp <- which(csm > e)
-    if(any(tmp)) { return(min(which(csm > e))) } else { return (0) }
+  if(length(p_no_1)>0)
+  {
+    
+    ## we approximate the empirical cdf by stepsize precision, over first n_steps
+    ## steps
+    
+    x <- seq(from = 0, by = precision, length = n_steps)
+    csm <- cumsum(f(x, ...)) * precision
+    iter <- 0
+    
+    
+    ## if we didn't go far enough to catch the quantile p (or at least one of
+    ## them) we reiterate the process as long as needed, by n_steps at a time
+    
+    while (max(p_no_1) > max(csm)) {
+      iter <- iter + 1
+      new_x <- seq(from = max(x)+precision, by = precision, length = n_steps)
+      x <- c(x, new_x)
+      new_cms <- cumsum(f(new_x, ...))*precision + max(csm)
+      csm <- c(csm, new_cms)
+    }
+    
+    
+    ## now we have the approximate cdf up to far enough, we find where the
+    ## quantile(s) p lie in the recorded vector of cdf quantile defined as the
+    ## smallest recorded cdf which is > p
+    
+    idx_q <- vapply(p_no_1, function(e){
+      tmp <- which(csm > e)
+      if(any(tmp)) { return(min(which(csm > e))) } else { return (0) }
     }
     , 0L)
-  out <- x[idx_q]
-  ## check <- csm[idx_q] ## compared to p
+    out_no_1 <- x[idx_q]
+    ## check <- csm[idx_q] ## compared to p_no_1
+    
+    out[p < 1] <- out_no_1
+    
+  }
   
   return(out)
   
